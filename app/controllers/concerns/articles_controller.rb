303727ddf2f3963df323d902a32f400b28afa701
sebagarcia <sebastian.garcia@alumnos.usm.cl>
2016-02-25 10:41:31 -0300

    video 9 de codigo facilito enviar datos al controlador

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -5,6 +5,20 @@ class ArticlesController < ApplicationController
 	end
 	#GET/articles/id
 	def show
-		@articles = Article.find(params[:id]) 
+		@article = Article.find(params[:id]) 
+	end
+	#GET/articles/new
+	def new 
+		@article = Article.new
+	end
+	#POST /articles
+	def create
+		@article = Article.new(title: params[:article][:title],
+													body: params[:article][:body])
+		@article.save
+		redirect_to @article
+	end
+	#UPDATE /articles
+	def update
 	end
 end
