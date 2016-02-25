861ee12b50bc23d3141074b58ff78db665107953
sebagarcia <sebastian.garcia@alumnos.usm.cl>
2016-02-25 14:57:24 -0300

    vide 12, article_params seguridad en los new

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -13,8 +13,7 @@ class ArticlesController < ApplicationController
 	end
 	#POST /articles
 	def create
-		@article = Article.new(title: params[:article][:title],
-													body: params[:article][:body])
+		@article = Article.new article_params
 		if @article.save
 			redirect_to @article
 		else
@@ -28,5 +27,8 @@ class ArticlesController < ApplicationController
 	end
 	#UPDATE /articles
 	def update
+	end
+	def article_params
+		params.require(:article).permit(:title,:body)
 	end
 end
