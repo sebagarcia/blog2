f82bb6f4f1a933cd298ac77acefadae5dd8a2660
sebagarcia <sebastian.garcia@alumnos.usm.cl>
2016-02-26 17:12:16 -0300

    video 16 de codigofacilito, asociacion de 1 a muchos

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
 	#POST /articles
 	def create
-		@article = Article.new article_params
+		@article = current_user.articles.new article_params
 		if @article.save
 			redirect_to @article
 		else
